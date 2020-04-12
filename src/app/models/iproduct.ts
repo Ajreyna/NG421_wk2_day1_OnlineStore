ecbdf600c3b0388af132585d013338fd23191a65
ajreyna <ajreyna88@gmail.com>
2020-04-12 14:16:35 -0500

    3rd commit

---
+++ src/app/models/iproduct.ts
@@ -0,0 +1,13 @@
+import { ExecFileOptionsWithStringEncoding } from 'child_process';
+
+export interface IProduct {
+
+    id: number;
+    name: string;
+    description: string;
+    reviews: number;
+    rating: number;
+    price: string;
+    imgUrl: string;
+
+}
